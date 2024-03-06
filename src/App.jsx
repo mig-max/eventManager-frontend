2072053397e229714c151773d2e04a5b82d838cb
Max Wellings <maxmechlin@gmail.com>
2024-03-06 21:54:45 +0100

    login and home imports and routes added back to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import {Routes, Route} from "react-router-dom";
-
+import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
 
 function App() {
  
@@ -8,6 +9,10 @@ function App() {
   return (
     <>
      <h1>Our Project</h1>
+     <Routes>
+        <Route path='/' element={<HomePage/>} />
+        <Route path='/login' element={<LoginPage/>} />
+     </Routes>
     </>
   )
 }
