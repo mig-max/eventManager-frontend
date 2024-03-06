735d14c2b02ab045b74fd19caed6c80fdfa1a4cb
Miguel Chito <96595540+donxito@users.noreply.github.com>
2024-03-07 00:33:38 +0100

    Conected signup page with login and profile page. tailwind and daisyUi init

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import './App.css'
 import {Routes, Route} from "react-router-dom";
+
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
 import SignupPage from './pages/SignupPage';
@@ -11,6 +11,7 @@ function App() {
   return (
     <>
      <h1>This is the app and: </h1>
+
      <Routes>
         <Route path='/' element={<HomePage/>} />
         <Route path='/signup' element={<SignupPage/>} />
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js',
+  ],
+  
+  plugins: [require('daisyui')],
+
+}
