6cf798d93b06a944c02e4c1e5aac010714a2e52b
Miguel Chito <96595540+donxito@users.noreply.github.com>
2024-03-09 13:13:40 +0100

    create IsOwner component. update Navbar component

--- src/components/IsLoggedIn.jsx
+++ src/components/IsLoggedIn.jsx
@@ -5,13 +5,15 @@ import { Navigate } from "react-router-dom";
 
 function IsLoggedIn({children}) {
 
-    const{isLoggedIn, isLoading} = useContext(AuthContext);
+    const{isLoggedIn, isLoading, user} = useContext(AuthContext);
 
+    
     if (isLoading) return <p>Loading...</p>;
 
+    // if the user is not logged in, redirect to login page
     if (!isLoggedIn) {
         return <Navigate to="/login" />;
-    } else {
+    } else {  // else can stay
         return children;
     }
 
